e2bc039bcc59459177e88d2eb3fef05876493999
Chrystalio <chrystalio.k@outlook.com>
2025-05-15 22:48:31 +0700

    Implement table component to index and Load data to table

--- resources/js/pages/tasks/index.tsx
+++ resources/js/pages/tasks/index.tsx
@@ -1,12 +1,39 @@
 import AppLayout from '@/layouts/app-layout';
 import { Head } from '@inertiajs/react';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { Task } from '@/types';
 
-export default function Index(){
+export default function Index({tasks}: { tasks: Task[] }){
     return (
         <AppLayout>
             <Head title="Tasks List" />
             <div>
-                List
+                <Table className={'mt-4'} >
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Task</TableHead>
+                            <TableHead className="w-[100px]">Status</TableHead>
+                            <TableHead className="w-[150px] text-right">Actions</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {tasks.map((task) =>(
+                            <TableRow key={task.id}>
+                                <TableCell>{task.name}</TableCell>
+                                <TableCell className={task.is_completed ? 'text-green-600' : 'text-red-700'}>
+                                    {task.is_completed ? 'Completed' : 'In Progress'}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
             </div>
         </AppLayout>
     )
