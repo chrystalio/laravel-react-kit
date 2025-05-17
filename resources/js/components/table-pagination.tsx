a52e00de1bddd019a25f89f6f38759dfbceb11b7
Chrystalio <chrystalio.k@outlook.com>
2025-05-18 01:33:17 +0700

    Integrate generatePaginationLinks into TablePagination component

--- resources/js/components/table-pagination.tsx
+++ resources/js/components/table-pagination.tsx
@@ -1,4 +1,5 @@
 import { Pagination, PaginationContent, PaginationItem, PaginationNext, PaginationPrevious } from '@/components/ui/pagination';
+import { generatePaginationLinks } from '@/lib/generate-pagination-links';
 import { type PaginatedResponse } from '@/types';
 
 export function TablePagination({ resource }: { resource: PaginatedResponse }) {
@@ -16,6 +17,9 @@ export function TablePagination({ resource }: { resource: PaginatedResponse }) {
                 <PaginationItem>
                     {resource.prev_page_url ? <PaginationPrevious href={resource.prev_page_url} /> : null }
                 </PaginationItem>
+
+                {generatePaginationLinks(resource.current_page, resource.last_page, resource.path, resource.links)}
+
                 <PaginationItem>
                     {resource.next_page_url ? <PaginationNext href={resource.next_page_url} /> : null }
                 </PaginationItem>
