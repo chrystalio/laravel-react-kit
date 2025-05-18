f2b2634ae8cad3055cda8d7c821d9db85ae9e661
Chrystalio <chrystalio.k@outlook.com>
2025-05-18 23:03:18 +0700

    Turns out the unused code wasnt actually unused after all

--- resources/js/components/table-pagination.tsx
+++ resources/js/components/table-pagination.tsx
@@ -3,6 +3,13 @@ import { generatePaginationLinks } from '@/lib/generate-pagination-links';
 import { type PaginatedResponse } from '@/types';
 
 export function TablePagination({ resource }: { resource: PaginatedResponse }) {
+    if (resource.last_page === 1) {
+        return (
+            <div className={ 'mt-4 text-center text-gray-500' }>
+                No Data
+            </div>
+        );
+    }
     return (
         <Pagination className='mt-4'>
             <PaginationContent>
