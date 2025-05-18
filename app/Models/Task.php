e6e2444a56f50aeab3d7fd27d5fe7bc1752db908
Chrystalio <chrystalio.k@outlook.com>
2025-05-18 15:53:36 +0700

    Add appends and getMediaFileAttribute method

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,18 +3,26 @@
 namespace App\Models;
 
 use Database\Factories\TaskFactory;
+use Illuminate\Console\Concerns\InteractsWithIO;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Task extends Model
+class Task extends Model implements HasMedia
 {
     /** @use HasFactory<TaskFactory> */
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $fillable = [
         'name',
         'is_completed',
         'due_date'
+    ];
+
+    protected $appends = [
+        'mediaFile'
     ];
 
     protected function cast(): array
@@ -23,5 +31,13 @@ class Task extends Model
             'is_completed' => 'boolean',
             'due_date' => 'date'
         ];
+    }
+
+    public function getMediaFileAttribute() {
+        if ($this->relationLoaded('media')) {
+            return $this->getFirstMedia();
+        }
+
+        return null;
     }
 }
