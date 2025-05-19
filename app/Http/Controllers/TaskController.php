5d15a8067c3272ecd4abc72ac8eb0fed2bcd5178
chrystalio <chrystalio.k@outlook.com>
2025-05-19 23:24:59 +0700

    Add category sync on task create/update and category filter on task index

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,29 +5,43 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
+use App\Models\TaskCategory;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Http\Controllers\TaskCategoryController;
 
 class TaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
-    {
-        return Inertia::render('tasks/index', [
-            'tasks' => Task::with('media')->paginate(10),
-        ]);
-    }
+    public function index(Request $request)
+{
+    return Inertia::render('tasks/index', [
+        'tasks' => Task::query()
+            ->with(['media', 'taskCategories'])
+            ->when($request->has('categories'), function ($query) use ($request) {
+                $query->whereHas('taskCategories', function ($query) use ($request) {
+                    $query->whereIn('id', $request->query('categories'));
+                });
+            })
+            ->paginate(20)
+            ->withQueryString(),
+        'categories' => TaskCategory::whereHas('tasks')->withCount('tasks')->get(),
+        'selectedCategories' => $request->query('categories'),
+    ]);
+}
 
     /**
      * Show the form for creating a new resource.
      */
     public function create(): Response
     {
-        return Inertia::render('tasks/create');
+        return Inertia::render('tasks/create', [
+            'categories' => TaskCategory::all(),
+        ]);
     }
 
     /**
@@ -36,10 +50,14 @@ class TaskController extends Controller
     public function store(StoreTaskRequest $request): RedirectResponse
     {
         /** @var \Illuminate\Http\Request $request */
-        $task = Task::create($request->validated() + ['is_completed' => false]);
+        $task = Task::create($request->safe(['name', 'due_date']) + ['is_completed' => false]);
 
         if ($request->hasFile('media')) {
             $task->addMedia($request->file('media'))->toMediaCollection();
+        }
+
+        if ($request->has('categories')) {
+            $task->taskCategories()->sync($request->validated('categories'));
         }
 
         return redirect()->route('tasks.index');
@@ -50,11 +68,12 @@ class TaskController extends Controller
      */
     public function edit(Task $task): Response
     {
-        $task->load(['media']);
+        $task->load(['media', 'TaskCategories']);
         $task->append('mediaFile');
 
         return Inertia::render('tasks/edit', [
             'task' => $task,
+            'categories' => TaskCategory::all(),
         ]);
     }
 
@@ -70,6 +89,8 @@ class TaskController extends Controller
             $task->getFirstMedia()?->delete();
             $task->addMedia($request->file('media'))->toMediaCollection();
         }
+
+        $task->taskCategories()->sync($request->validated('categories', []));
 
         return redirect()->route('tasks.index');
     }
