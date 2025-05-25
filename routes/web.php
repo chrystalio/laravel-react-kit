b3a9cc6ff6469e6f18c84fbcefdce94d72918ed4
chrystalio <chrystalio.k@outlook.com>
2025-05-25 21:39:48 +0700

    Add dashboard routes for task status and stats summary

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TaskCategoryController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
@@ -13,6 +14,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::controller(DashboardController::class)->prefix('dashboard')->group(function () {
+        Route::get('task-status', 'getTaskStatusDistribution');
+        Route::get('stats', 'getStatsSummary');
+    });
 
     Route::resource('tasks', TaskController::class);
     Route::resource('task-categories', TaskCategoryController::class);
